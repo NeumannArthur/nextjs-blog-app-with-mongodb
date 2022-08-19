f1e302b1477158197b1ed14ba758a3cd01775a76
Arthur <art.neumann@outlook.com>
2022-08-19 15:58:33 +0200

    removed data fetching from SSP

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-
+import { MongoClient } from 'mongodb';
 import Nav from '../components/Nav';
 import PostCard from '../components/PostCard';
 import styles from '../styles/Home.module.css';
@@ -31,15 +31,27 @@ export default function Home({ posts }) {
 }
 
 export async function getServerSideProps(ctx) {
-    let dev = process.env.NODE_ENV !== 'production';
-    let { DEV_URL, PROD_URL } = process.env;
+    // let dev = process.env.NODE_ENV !== 'production';
+    // let { DEV_URL, PROD_URL } = process.env;
 
-    let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/posts`);
-    let data = await response.json();
+    // let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/posts`);
+    // let data = await response.json();
+
+    const client = await MongoClient.connect('mongodb+srv://Arthur:Mon130541Beiertheim@cluster0.qmdtv.mongodb.net/sample_posts?retryWrites=true&w=majority');
+    const db = client.db();
+    const data = await db.collection('posts');
+    const post_list = await data.find().toArray();
+    // console.log(post_list);
+
 
     return {
         props: {
-            posts: data['message'],
+            posts: post_list.map((post) => ({
+                id: post._id.toString(),
+                title: post.title,
+                content: post.content,
+                createdAt: post.createdAt
+            }))
         },
     };
 }
